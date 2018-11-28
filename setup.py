827fb5429465c8a03941202495169c43e022e43f
Saurabh Gharat <37373648+saurabhgharat@users.noreply.github.com>
2018-11-28 21:26:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 
 from distutils.core import setup
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(
   name = 'image-compression',        
   packages = ['image-compression'],   
@@ -7,7 +12,7 @@ setup(
   license='MIT',        
   description = 'compress image without losing quality',  
   long_description=long_description,
-+ long_description_content_type="text/markdown",
+  long_description_content_type='text/markdown'
   author = 'Saurabh Gharat',                   
   author_email = 'saurabhhgharat9@gmail.com',      
   url = 'https://github.com/saurabhgharat',   
