3165b636194881afb459b9cc98d22bcfc99492c1
Saurabh Gharat <37373648+saurabhgharat@users.noreply.github.com>
2018-12-10 15:32:34 +0530

    Update img.py

--- image-compression/img.py
+++ image-compression/img.py
@@ -1,21 +1,27 @@
 from PIL import Image
 def compress_image():
-    with Image.open('image_name.jpeg','r') as im: # Open the image in Read Mode
-         pix_val = list(im.getdata())  # get pixel value in RGB format
+    im=Image.open('image_name.jpeg','r') # Open the image in Read Mode
+    pix_val = list(im.getdata())  # get pixel value in RGB format
 
     a= [x for sets in pix_val for x in sets] #Convert list of tuples into one list 
 
     myRoundedList =  [round(x,-1) for x in a]  #Round integers to nearest 10
-
-    b=[tuple(myRoundedList[i:i+3]) for i in range(0, len(myRoundedList), 3)]  #Group list to a tuple of 3 integers 
-
+    if im.mode in("RGBA","p"):
+        b=[tuple(myRoundedList[i:i+4]) for i in range(0, len(myRoundedList), 4)]  #Group list to a tuple of 4 integers
+    elif im.mode in("RGB"):
+        b=[tuple(myRoundedList[i:i+3]) for i in range(0, len(myRoundedList), 3)]   #Group list to a tuple of 3 integers
+   
     list_of_pixels = list(b)
 
     im2 = Image.new(im.mode, im.size) #Create a new image 
 
     im2.putdata(list_of_pixels) #put image data into the new image 
-
-    im2.save('new_image_name.jpeg')  #save the file 
+    
+    if im.mode in("RGBA","p"):           #save the file 
+        im2.save(image_path,"PNG")
+    elif im.mode in("RGB"):
+        im2.save(image_path,"JPEG")
+      #save the file 
 
 
 if __name__ == "__main__":
